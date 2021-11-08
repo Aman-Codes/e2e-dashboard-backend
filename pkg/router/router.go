9bd53e8cbe892b9cf87715ebdc137ee1120914a2
Aman Dwivedi <aman.dwivedi5@gmail.com>
2021-11-08 08:37:57 +0530

    create routes for all github apis used in frontend
    Signed-off-by: Aman Dwivedi <aman.dwivedi5@gmail.com>

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"github.com/Aman-Codes/e2e-dashboard-backend/pkg/customErrors"
 	"github.com/Aman-Codes/e2e-dashboard-backend/pkg/fetchLog"
+	"github.com/Aman-Codes/e2e-dashboard-backend/pkg/sendRequest"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,5 +15,27 @@ func Router() {
 		})
 	})
 	router.GET("/runs/logs/:id", fetchLog.FetchLogApi)
+	router.GET("/repos/:orgName/litmus-e2e/actions/workflows", func(c *gin.Context) {
+		orgName := c.Param("orgName")
+		sendRequest.SendGetRequestWrapper(c, sendRequest.BaseGitHubUrl+"/repos/"+orgName+"/litmus-e2e/actions/workflows")
+	})
+	router.GET("/repos/:orgName/litmus-e2e/actions/runs/:pipelineId/jobs", func(c *gin.Context) {
+		orgName := c.Param("orgName")
+		pipelineId := c.Param("pipelineId")
+		sendRequest.SendGetRequestWrapper(c, sendRequest.BaseGitHubUrl+"/repos/"+orgName+"/litmus-e2e/actions/runs/"+pipelineId+"/jobs")
+	})
+	router.GET("/repos/:orgName/litmus-e2e/actions/runs", func(c *gin.Context) {
+		orgName := c.Param("orgName")
+		sendRequest.SendGetRequestWrapper(c, sendRequest.BaseGitHubUrl+"/repos/"+orgName+"/litmus-e2e/actions/runs")
+	})
+	router.GET("/repos/:orgName/litmus-e2e/actions/workflows/:workflowName/runs", func(c *gin.Context) {
+		orgName := c.Param("orgName")
+		workflowName := c.Param("workflowName")
+		sendRequest.SendGetRequestWrapper(c, sendRequest.BaseGitHubUrl+"/repos/"+orgName+"/litmus-e2e/actions/workflows/"+workflowName+"/runs")
+	})
+	router.GET("/repos/:orgName/litmus-go/commits", func(c *gin.Context) {
+		orgName := c.Param("orgName")
+		sendRequest.SendGetRequestWrapper(c, sendRequest.BaseGitHubUrl+"/repos/"+orgName+"/litmus-go/commits")
+	})
 	router.Run(":8080")
 }
